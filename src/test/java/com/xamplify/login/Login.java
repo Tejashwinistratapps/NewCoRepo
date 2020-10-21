1d1a1e065e71556b00bb55cdca42cc6509ff4573
Tejashwinistratapps <44365003+Tejashwinistratapps@users.noreply.github.com>
2020-10-21 11:58:55 +0530

    New cloned project

---
+++ src/test/java/com/xamplify/login/Login.java
@@ -0,0 +1,37 @@
+package com.xamplify.login;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	 WebDriver driver = Instance.getInstance();
+
+		Properties prop = PropertiesFile.readPropertyFile("rdata.properties");
+	@Test(priority=1)
+	public void start() throws InterruptedException {
+		
+	driver.get(prop.getProperty("baseUrl"));
+	
+	driver.manage().window().maximize();
+	Thread.sleep(3000);
+	}
+
+	@Test(priority=2,enabled=true)
+	public void login() throws InterruptedException, SQLException {
+
+		
+		Thread.sleep(5000);
+		/*driver.findElement(By.id(prop.getProperty("email"))).sendKeys(prop.getProperty("user.name"));
+		driver.findElement(By.id(prop.getProperty("password"))).sendKeys(prop.getProperty("user.password"));
+		driver.findElement(By.id(prop.getProperty("signin"))).click();*/
+		Thread.sleep(15000);
+
+	}
+			
+		
+			
+	
+}
